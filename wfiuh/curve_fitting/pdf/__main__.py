e578e5ac84a10ffbd8f866e8f2652612f0f7ffc3
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-12-05 18:00:13 +0800

    refactor(curve_fitting/pdf): refactor file structure

--- wfiuh/curve_fitting/pdf/__main__.py
+++ wfiuh/curve_fitting/pdf/__main__.py
@@ -1,52 +1,43 @@
 import concurrent.futures
 import glob
+import os
 import signal
 import sys
-import threading
-import typing
 
 import pandas as pd
 import rich.console
 import rich.live
 import rich.panel
+import rich.pretty
 import rich.progress
-import scipy.optimize
-import sklearn.metrics
 
-from . import models
-
-interrupt_event = threading.Event()
-
-
-def handle_sigint(signal_num, frame):
-    interrupt_event.set()
-
-
-signal.signal(signal.SIGINT, handle_sigint)
+from ..typed import Model
+from . import (
+    Beta,
+    DoubleTriangular,
+    Hoerl,
+    InverseGaussian,
+    NormalGaussian,
+    Polynomial,
+    curve_fitting_file,
+)
 
 
-def curve_fitting(
-    filepath: str,
-    f: typing.Callable,
-) -> dict | None:
-    if interrupt_event.is_set():
-        return None
-    try:
-        df = pd.read_csv(filepath)
-        x = df["flowTime"]
-        y = df["frequency"]
-        res = scipy.optimize.curve_fit(f=f, xdata=x, ydata=y)
-    except RuntimeError as e:
-        raise type(e)(f"{filepath}: {e}")
-    else:
-        popt, pcov = res
-        r2_score = sklearn.metrics.r2_score(y_true=y, y_pred=f(x, *popt))
-        return {"filepath": filepath, "popt": popt, "r2_score": r2_score}
+def initializer():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    sys.stderr = open(file=os.devnull, mode="w")
 
 
 def main(
     prefix: str = "2-sub-WFIUH_rescaled",
-    model_names: list[str] = ["normal_gaussian", "inverse_gaussian", "polynomial"],
+    models: list[Model] = [
+        Beta(),
+        DoubleTriangular(),
+        Hoerl(),
+        InverseGaussian(),
+        NormalGaussian(),
+        Polynomial(),
+    ],
 ) -> int:
     files = glob.glob(pathname=f"{prefix}/*.csv")
     overall_progress = rich.progress.Progress(
@@ -73,25 +64,26 @@ def main(
     )
     with rich.live.Live(progress_group) as live:
         overview_task_id = overall_progress.add_task(description="Overall Progress")
-        for model_name in overall_progress.track(model_names, task_id=overview_task_id):
+        for model in overall_progress.track(models, task_id=overview_task_id):
             model_task_id = models_progress.add_task(
-                description=model_name, total=len(files)
+                description=model.name, total=len(files)
             )
             rets: list[dict] = []
             try:
-                f = getattr(models, model_name)
-                with concurrent.futures.ProcessPoolExecutor() as pool:
+                with concurrent.futures.ProcessPoolExecutor(
+                    initializer=initializer
+                ) as pool:
                     futures: list[concurrent.futures.Future] = list(
                         map(
                             lambda filepath: pool.submit(
-                                curve_fitting, filepath=filepath, f=f
+                                curve_fitting_file,
+                                model=model,
+                                filepath=filepath,
                             ),
                             files,
                         )
                     )
                     for future in concurrent.futures.as_completed(futures):
-                        if interrupt_event.is_set():
-                            raise KeyboardInterrupt()
                         try:
                             ret = future.result()
                             if ret:
@@ -101,17 +93,23 @@ def main(
                             raise e
                         except Exception as e:
                             live.console.log(
-                                f"{model_name}:", e, style="logging.level.error"
+                                model.name,
+                                rich.pretty.Pretty(e),
+                                style="logging.level.error",
                             )
             except KeyboardInterrupt as e:
-                results = pd.DataFrame(rets)
-                results.to_csv(f"{model_name}.csv")
+                results = pd.DataFrame.from_records(data=rets, index="id")
+                results.sort_index(inplace=True)
+                results.to_csv(f"{model.name}.csv")
                 raise e
             except Exception as e:
-                live.console.log(f"{model_name}:", e, style="logging.level.error")
+                live.console.log(
+                    model.name, rich.pretty.Pretty(e), style="logging.level.error"
+                )
             else:
-                results = pd.DataFrame(rets)
-                results.to_csv(f"{model_name}.csv")
+                results = pd.DataFrame.from_records(data=rets, index="id")
+                results.sort_index(inplace=True)
+                results.to_csv(f"{model.name}.csv")
     return 0
 
 
